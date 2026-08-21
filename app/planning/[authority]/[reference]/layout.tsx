d8fdaaf0a15d4b00aa626328f2e99e684e6b03eb
danjspg <dannyspillane@gmail.com>
2026-08-21 09:39:11 +0100

    Tighten desktop planning lifecycle spacing

--- app/planning/[authority]/[reference]/layout.tsx
+++ app/planning/[authority]/[reference]/layout.tsx
@@ -16,26 +16,37 @@ export default function PlanningApplicationLayout({
 
           .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child {
             display: grid;
-            grid-template-columns: minmax(0, 1.25fr) minmax(0, 1fr) minmax(14rem, 0.8fr);
+            grid-template-columns: minmax(0, 1.15fr) minmax(0, 1.05fr) minmax(15rem, 0.9fr);
             grid-auto-flow: row;
-            column-gap: 1.75rem;
+            column-gap: 2rem;
             row-gap: 0.35rem;
             align-items: start;
           }
 
-          .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child > p:nth-child(1),
-          .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child > p:nth-child(2),
+          .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child > p:nth-child(1) {
+            grid-column: 1;
+            grid-row: 1;
+          }
+
+          .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child > p:nth-child(2) {
+            grid-column: 1;
+            grid-row: 2;
+          }
+
           .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child > p:nth-child(3) {
             grid-column: 1;
+            grid-row: 3;
           }
 
           .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child > div {
             grid-column: 2;
+            grid-row: 1 / span 4;
+            align-self: stretch;
             margin-top: 0 !important;
             border-top: 0 !important;
             border-left: 1px solid rgb(231 229 228);
             padding-top: 0 !important;
-            padding-left: 1.75rem;
+            padding-left: 2rem;
           }
 
           .planning-application-layout > main > section:first-of-type > div > div:last-child > div:last-child > a {
