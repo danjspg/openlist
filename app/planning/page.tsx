c29fdf2287847f8cdcccd8c48e10370bf3ec2b5e
danjspg <dannyspillane@gmail.com>
2026-08-28 18:17:15 +0100

    Promote Planning category links on homepage

--- app/planning/page.tsx
+++ app/planning/page.tsx
@@ -1,5 +1,13 @@
-export {
-  default,
-  metadata,
-  revalidate,
-} from "@/app/planning/applications/PlanningApplicationsPage"
+import { PlanningApplicationsView, metadata, revalidate } from "@/app/planning/applications/PlanningApplicationsPage"
+import { PlanningCategoryLinks } from "@/components/planning/PlanningCategoryLinks"
+
+export { metadata, revalidate }
+
+export default async function PlanningPage() {
+  return (
+    <>
+      <PlanningCategoryLinks />
+      <PlanningApplicationsView />
+    </>
+  )
+}
