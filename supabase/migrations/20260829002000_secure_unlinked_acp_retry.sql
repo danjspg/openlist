9026da9d0ec084ac00c07cb1e53b3ac724fc54bc
danjspg <dannyspillane@gmail.com>
2026-08-29 01:07:14 +0100

    Restrict ACP retry RPC to service role

---
+++ supabase/migrations/20260829002000_secure_unlinked_acp_retry.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.openlist_requeue_matchable_unlinked_acp_cases(integer) from public, anon, authenticated;
+grant execute on function public.openlist_requeue_matchable_unlinked_acp_cases(integer) to service_role;
