418085b2f87a8cd4d7bfe1c57bb47ff9428363b3
danjspg <dannyspillane@gmail.com>
2026-09-04 20:59:19 +0100

    Add signed-in routes to planning area alerts (#178)
    * Add signed-in planning alert discovery CTA
    
    * Mount private alert discovery on planning homepage
    
    * Mount planning alert discovery entry
    
    * Add signed-in area alerts to planning mega menu

--- app/planning/layout.tsx
+++ app/planning/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react"
 import { PrivatePlanningAreaAlertEntry } from "@/components/planning/PrivatePlanningAreaAlertEntry"
+import { PrivatePlanningAlertDiscovery } from "@/components/planning/PrivatePlanningAlertDiscovery"
 
 export default function PlanningLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <PrivatePlanningAreaAlertEntry />
+      <PrivatePlanningAlertDiscovery />
     </>
   )
 }
